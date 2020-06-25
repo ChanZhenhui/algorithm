dbd6525f0ef288d844541351fd677fc237ae648a
knight <chan_zhenhui@outlook.com>
2020-06-25 14:04:03 +0800

    first commit

--- src/com/chan/sort/InsertSort.java
+++ src/com/chan/sort/InsertSort.java
@@ -2,8 +2,22 @@ package com.chan.sort;
 
 public class InsertSort {
 
-    public static int[] insertSort(int[] arr) {
-
-        return arr;
+    /**
+     * 插入排序，前面有序，后面无序，将无序的插入到有序序列当中
+     *
+     * @param arr
+     */
+    public static void insertSort(int[] arr) {
+        int insertVal;
+        int insertIndex;
+        for (int i = 1; i < arr.length; i++) {
+            insertVal = arr[i];
+            insertIndex = i - 1;
+            while (insertIndex >= 0 && insertVal < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex --;
+            }
+            arr[insertIndex + 1] = insertVal;
+        }
     }
 }
